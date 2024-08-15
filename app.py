fe7d37c75c956f8fd4ddf01a528a7ca9ef1796ca
Rimjhim Mittal <rmittal_be21@thapar.edu>
2024-08-15 11:49:06 +0530

    Stateless Stateful check

--- app.py
+++ app.py
@@ -17,67 +17,79 @@ def reset_simulation_state():
     if 'selected_columns' in st.session_state:
         del st.session_state.selected_columns
 
-def run_simulation(param_inputs, mdf_model):
+def run_simulation(param_inputs, mdf_model, stateful):
     mod_graph = mdf_model.graphs[0]
     nodes = mod_graph.nodes
-    
-    duration = param_inputs["Simulation Duration (s)"]
-    dt = param_inputs["Time Step (s)"]
-    
     all_node_results = {}
+    if stateful:
+        duration = param_inputs["Simulation Duration (s)"]
+        dt = param_inputs["Time Step (s)"]
     
-    for node in nodes:
-        eg = EvaluableGraph(mod_graph, verbose=False)
-        t = 0
-        times = []
-        node_outputs = {op.value : [] for op in node.output_ports}
-        node_outputs['Time'] = []
         
-        while t <= duration:
-            times.append(t)
-            if t == 0:
-                eg.evaluate()
-            else:
-                eg.evaluate(time_increment=dt)
-
-            node_outputs['Time'].append(t)
-            for op in node.output_ports:
-                eval_param = eg.enodes[node.id].evaluable_outputs[op.id]
-                output_value = eval_param.curr_value
-                if isinstance(output_value, (list, np.ndarray)):
-                    scalar_value = output_value[0] if len(output_value) > 0 else np.nan
-                    node_outputs[op.value].append(float(scalar_value))
+        
+        for node in nodes:
+            eg = EvaluableGraph(mod_graph, verbose=False)
+            t = 0
+            times = []
+            node_outputs = {op.value : [] for op in node.output_ports}
+            node_outputs['Time'] = []
+            
+            while t <= duration:
+                times.append(t)
+                if t == 0:
+                    eg.evaluate()
                 else:
-                    node_outputs[op.value].append(float(output_value))
-            t += dt
-        
-        all_node_results[node.id] = pd.DataFrame(node_outputs).set_index('Time')
-    
-    return all_node_results
+                    eg.evaluate(time_increment=dt)
 
-def show_simulation_results(all_node_results):
+                node_outputs['Time'].append(t)
+                for op in node.output_ports:
+                    eval_param = eg.enodes[node.id].evaluable_outputs[op.id]
+                    output_value = eval_param.curr_value
+                    if isinstance(output_value, (list, np.ndarray)):
+                        scalar_value = output_value[0] if len(output_value) > 0 else np.nan
+                        node_outputs[op.value].append(float(scalar_value))
+                    else:
+                        node_outputs[op.value].append(float(output_value))
+                t += dt
+            
+            all_node_results[node.id] = pd.DataFrame(node_outputs).set_index('Time')
+        
+        return all_node_results
+    else:
+        for node in nodes:
+            eg = EvaluableGraph(mod_graph, verbose=False)
+            eg.evaluate()
+            all_node_results[node.id] = pd.DataFrame({op.value: [float(eg.enodes[node.id].evaluable_outputs[op.id].curr_value)] for op in node.output_ports})
+            
+    return all_node_results
+def show_simulation_results(all_node_results, stateful_nodes):
     if all_node_results is not None:
         for node_id, chart_data in all_node_results.items():
-            st.subheader(f"Simulation Results for Node: {node_id}")
-            if 'selected_columns' not in st.session_state:
-                st.session_state.selected_columns = {node_id: {col: True for col in chart_data.columns}}
-            elif node_id not in st.session_state.selected_columns:
-                st.session_state.selected_columns[node_id] = {col: True for col in chart_data.columns}
-            columns = chart_data.columns
-            for column in columns:
-                st.checkbox(
-                    f"{column}",
-                    value=st.session_state.selected_columns[node_id][column],
-                    key=f"checkbox_{node_id}_{column}",
-                    on_change=update_selected_columns,
-                    args=(node_id, column,)
-                )
+            st.subheader(f"Results for Node: {node_id}")
+            if node_id in stateful_nodes:
+                if 'selected_columns' not in st.session_state:
+                    st.session_state.selected_columns = {node_id: {col: True for col in chart_data.columns}}
+                elif node_id not in st.session_state.selected_columns:
+                    st.session_state.selected_columns[node_id] = {col: True for col in chart_data.columns}
+                columns = chart_data.columns
+                for column in columns:
+                    st.checkbox(
+                        f"{column}",
+                        value=st.session_state.selected_columns[node_id][column],
+                        key=f"checkbox_{node_id}_{column}",
+                        on_change=update_selected_columns,
+                        args=(node_id, column,)
+                    )
 
-            # Filter the data based on selected checkboxes
-            filtered_data = chart_data[[col for col, selected in st.session_state.selected_columns[node_id].items() if selected]]
+                # Filter the data based on selected checkboxes
+                filtered_data = chart_data[[col for col, selected in st.session_state.selected_columns[node_id].items() if selected]]
 
-            # Display the line chart with filtered data
-            st.line_chart(filtered_data, use_container_width=True, height=400)
+                # Display the line chart with filtered data
+                st.line_chart(filtered_data, use_container_width=True, height=400)
+            else:
+                st.write(all_node_results[node_id])
+
+
 def update_selected_columns(node_id, column):
     st.session_state.selected_columns[node_id][column] = st.session_state[f"checkbox_{node_id}_{column}"]
 
@@ -92,16 +104,26 @@ def show_json_output(mdf_model):
     st.json(mdf_model.to_json())
 
 # st.cache_data()
-def view_tabs(mdf_model, param_inputs): # view
+def view_tabs(mdf_model, param_inputs, stateful): # view
     tab1, tab2, tab3 = st.tabs(["Simulation Results", "MDF Graph", "Json Model"])
     with tab1:
-        if 'simulation_results' not in st.session_state:
-            st.session_state.simulation_results = None
-            
-        if st.session_state.simulation_results is not None:
-            show_simulation_results(st.session_state.simulation_results)
+        if stateful:
+            if 'simulation_results' not in st.session_state:
+                st.session_state.simulation_results = None
+                
+            if st.session_state.simulation_results is not None:
+                show_simulation_results(st.session_state.simulation_results, stateful)
+            else:
+                st.write("Run the simulation to see results.") # model
         else:
-            st.write("Run the simulation to see results.") # model
+            if 'simulation_results' not in st.session_state:
+                st.session_state.simulation_results = None
+                
+            if st.session_state.simulation_results is not None:
+                show_simulation_results(st.session_state.simulation_results, stateful)
+            else:
+                st.write("Stateless.")
+
     with tab2:
         show_mdf_graph(mdf_model) # view
     with tab3:
@@ -131,17 +153,28 @@ def parameter_form_to_update_model_and_view(mdf_model):
     mod_graph = mdf_model.graphs[0]
     nodes = mod_graph.nodes
     parameters = []
+    stateful_nodes = []
+    stateful = False
+
     for node in nodes:
-        parameters.append(node.parameters)
+        for param in node.parameters:
+            if param.is_stateful():
+                stateful_nodes.append(node.id)
+                stateful = True
+                break
+            else:
+                stateful = False
+
     param_inputs = {}
-    if mdf_model.metadata:
-        preferred_duration = float(mdf_model.metadata.get("preferred_duration", 10))
-        preferred_dt = float(mdf_model.metadata.get("preferred_dt", 0.1))
-    else:
-        preferred_duration = 100
-        preferred_dt = 0.1
-    param_inputs["Simulation Duration (s)"] = preferred_duration
-    param_inputs["Time Step (s)"] = preferred_dt
+    if stateful:
+        if mdf_model.metadata:
+            preferred_duration = float(mdf_model.metadata.get("preferred_duration", 10))
+            preferred_dt = float(mdf_model.metadata.get("preferred_dt", 0.1))
+        else:
+            preferred_duration = 100
+            preferred_dt = 0.1
+        param_inputs["Simulation Duration (s)"] = preferred_duration
+        param_inputs["Time Step (s)"] = preferred_dt
 
     with st.form(key="parameter_form"):
         valid_inputs = True
@@ -178,26 +211,26 @@ def parameter_form_to_update_model_and_view(mdf_model):
                                 valid_inputs = False
                     
                     param_inputs[param.id] = value
-
-        st.write("Simulation Parameters:")
-        with st.container(border=True):
-            # Add Simulation Duration and Time Step inputs
-            col1, col2 = st.columns(2)
-            with col1:
-                sim_duration = st.text_input("Simulation Duration (s)", value=str(param_inputs["Simulation Duration (s)"]), key="sim_duration")
-            with col2:
-                time_step = st.text_input("Time Step (s)", value=str(param_inputs["Time Step (s)"]), key="time_step")
-            
-            try:
-                param_inputs["Simulation Duration (s)"] = float(sim_duration)
-            except ValueError:
-                st.error("Invalid input for Simulation Duration. Please enter a valid number.")
-                valid_inputs = False
-            try:
-                param_inputs["Time Step (s)"] = float(time_step)
-            except ValueError:
-                st.error("Invalid input for Time Step. Please enter a valid number.")
-                valid_inputs = False
+        if stateful:
+            st.write("Simulation Parameters:")
+            with st.container(border=True):
+                # Add Simulation Duration and Time Step inputs
+                col1, col2 = st.columns(2)
+                with col1:
+                    sim_duration = st.text_input("Simulation Duration (s)", value=str(param_inputs["Simulation Duration (s)"]), key="sim_duration")
+                with col2:
+                    time_step = st.text_input("Time Step (s)", value=str(param_inputs["Time Step (s)"]), key="time_step")
+                
+                try:
+                    param_inputs["Simulation Duration (s)"] = float(sim_duration)
+                except ValueError:
+                    st.error("Invalid input for Simulation Duration. Please enter a valid number.")
+                    valid_inputs = False
+                try:
+                    param_inputs["Time Step (s)"] = float(time_step)
+                except ValueError:
+                    st.error("Invalid input for Time Step. Please enter a valid number.")
+                    valid_inputs = False
 
         run_button = st.form_submit_button("Run Simulation")
         
@@ -207,12 +240,12 @@ def parameter_form_to_update_model_and_view(mdf_model):
                 for param in node.parameters:
                     if param.id in param_inputs:
                         param.value = param_inputs[param.id]
-            st.session_state.simulation_results = run_simulation(param_inputs, mdf_model)
+            st.session_state.simulation_results = run_simulation(param_inputs, mdf_model, stateful)
 
-    view_tabs(mdf_model, param_inputs)
+    view_tabs(mdf_model, param_inputs, stateful_nodes)
+
 def upload_file_and_load_to_model():
-    
-    
+   
     uploaded_file = st.sidebar.file_uploader("Choose a JSON/YAML/BSON file", type=["json", "yaml", "bson"])
     github_url = st.sidebar.text_input("Enter GitHub raw file URL:", placeholder="Enter GitHub raw file URL")
     example_models = {
@@ -281,7 +314,7 @@ def main():
         header1, header2 = st.columns([1, 8], vertical_alignment="top")
         with header1:
             with st.container():
-                st.image("logofinal.jpg")
+                st.image("logo.jpg")
         with header2:
             with st.container():
                 st.title("MDF: "+ mdf_model.id)
@@ -291,7 +324,7 @@ def main():
         header1, header2 = st.columns([1, 8], vertical_alignment="top")
         with header1:
             with st.container():
-                st.image("logofinal.jpg")
+                st.image("logo.jpg")
         with header2:
             with st.container():
                 st.title("Welcome to Model Description Format")
